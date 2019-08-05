3f2c04f853ada995728d027589a54dfe694b34ca
Thomas Lewis <TomPaul_67@yahoo.com>
2019-08-04 23:09:08 -0400

    small changes to firends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,8 @@
 var friends = [
 	/// the first is an example from instructions
 	{
-    "name":"Ahmed",
+    "name":"Alfonso",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   }
 ];

09078592ef4f08c2231a3b24e292b2e39bb5b4c2
Thomas Lewis <TomPaul_67@yahoo.com>
2019-08-05 10:39:07 -0400

    functioning server using a stub html file - home.html

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+console.log('HTML Routes Connected..');
+
+// Node Dependencies
+var path = require('path');
+
+module.exports = function(app) {
+	app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/survey.html"));
+	});
+
+	// homepage
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/home.html"));
+	});
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Server Routing Map
+//var apiRoutes = require('./app/routing/apiRoutes.js');
+var htmlRoutes = require('./app/routing/htmlRoutes.js');
+
+
+var app = express();
+
+// set the port, default to 8080 if no environment variable found.
+var PORT = process.env.PORT || 8081;
+
+// set up Express 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//apiRoutes(app);
+htmlRoutes(app);
+
+// Start listening
+app.listen(PORT, function() {
+    console.log("Friend Finder listening on PORT: " + PORT);
+  });

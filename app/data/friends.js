5e0d2fd6f2ae68abdf12dbecb58a9699574647b9
Thomas Lewis <TomPaul_67@yahoo.com>
2019-08-05 09:35:27 -0400

    added content to Friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,39 @@
 var friends = [
-	/// the first is an example from instructions
 	{
     "name":"Alfonso",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"http://clipart-library.com/images/pio5Ra7oT.png",
     "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    "name":"Owly",
+    "photo":"http://clipart-library.com/images/pc585nEqi.png",
+    "scores":[ 2, 3, 4, 1, 5, 3, 1, 1, 1, 2]
+  },
+  {
+    "name":"Stego",
+    "photo":"http://clipart-library.com/images/ki8nKEdbT.png",
+    "scores":[ 2, 3, 5, 1, 1, 2, 3, 4, 1, 3]
+  },
+  {
+    "name":"Segio",
+    "photo":"http://clipart-library.com/images/dc9X5B9Ki.png",
+    "scores":[ 3, 1, 5, 3, 2, 1, 4, 4, 3, 5]
+  },
+  {
+    "name":"Beedi",
+    "photo":"http://clipart-library.com/images/pcq8qn7Ki.png",
+    "scores":[ 1, 2, 1, 2, 1, 5, 5, 2, 4, 5]
+  },
+  {
+    "name":"Gomez",
+    "photo":"http://clipart-library.com/images/zcXednKoi.png",
+    "scores":[ 2, 3, 1, 1, 3, 3, 2, 4, 4, 2]
+  },
+  {
+    "name":"Ned",
+    "photo":"http://clipart-library.com/images/ki8nkd5ir.png",
+    "scores":[ 1, 1, 1, 1, 2, 3, 2, 2, 4, 5]
   }
+  /// for more: http://clipart-library.com/cartoon-animal-images.html
+
 ];

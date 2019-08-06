b8ddbd443b9504657119fc07ed723e48f036b78e
Thomas Lewis <tplewis@gmail.com>
2019-08-06 12:58:23 -0400

    survey.html started  - questions added  - submit button added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,14 +2,6 @@ console.log('API Routes Connected..');
 
 var friends = require("../data/friends.js");
 
-// var friends = [
-// 	{
-//     "name":"Alfonso",
-//     "photo":"http://clipart-library.com/images/pio5Ra7oT.png",
-//     "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-//   }
-// ]
-
 module.exports = function(app) {
     // get is basic, just return all friends
     app.get("/api/friends", function(req, res) {

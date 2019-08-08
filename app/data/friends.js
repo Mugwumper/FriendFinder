568a49df7fc7355288ac1ff66fbad1215c354ac3
Thomas Lewis <tplewis@gmail.com>
2019-08-08 12:25:10 -0400

    functioning  as expected  - added post method for new friend  - compare against existing and returns best match  - cleaned up interface to include descriptive values for each drop down control.  - added model and client side javascript to show best match.  - corrected all links to images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friends = [
 	{
     "name":"Alfonso",
-    "photo":"http://clipart-library.com/images/pio5Ra7oT.png",
+    "photo":"http://clipart-library.com/images/ki8nKEdbT.png",
     "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
     "name":"Owly",
-    "photo":"http://clipart-library.com/images/pc585nEqi.png",
+    "photo":"http://clipart-library.com/images/pc585nEqi.jpg",
     "scores":[ 2, 3, 4, 1, 5, 3, 1, 1, 1, 2]
   },
   {
@@ -16,17 +16,17 @@ var friends = [
   },
   {
     "name":"Segio",
-    "photo":"http://clipart-library.com/images/dc9X5B9Ki.png",
+    "photo":"http://clipart-library.com/images/BTaELBrGc.png",
     "scores":[ 3, 1, 5, 3, 2, 1, 4, 4, 3, 5]
   },
   {
     "name":"Beedi",
-    "photo":"http://clipart-library.com/images/pcq8qn7Ki.png",
+    "photo":"http://clipart-library.com/images/pcq8qn7Ki.jpg",
     "scores":[ 1, 2, 1, 2, 1, 5, 5, 2, 4, 5]
   },
   {
     "name":"Gomez",
-    "photo":"http://clipart-library.com/images/zcXednKoi.png",
+    "photo":"http://clipart-library.com/images/zcXednKoi.jpg",
     "scores":[ 2, 3, 1, 1, 3, 3, 2, 4, 4, 2]
   },
   {
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,51 @@ module.exports = function(app) {
         res.json(friends);
     });
 
+    // The app.post request handles when a user submits a form and thus submits data to the surver
+    app.post('/api/friends', function (req, res) {
+        // loop through all of the possible options
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
 
+        // To take the result of the user's survey POST and parse it
+        var userData = req.body;
+        var userScores = userData.scores;
+        //console.log(userData);
+        console.log("\nName: " + userData.name + "\nPhoto: " +
+                    userData.photo + "\nScores: " + userScores);
+
+        // The variable used to calculate the difference b/n the user's socres and the scores of each user
+        var totalDifference = 0;
+
+        //loop through the friends data array of objects to get each friends scores
+        for (var i = 0; i < friends.length; i++) {
+            var testFriend = friends[i];  
+            logStr = "examining results against friend #"+ (i+1) + " '" + testFriend.name + "'"; 
+            totalDifference = 0;
+            //loop through friends and calculate the absolute difference between the two
+            for (var j = 0; j < 10; j++) {
+                // We calculate the difference between the scores and sum them into the totalDifference
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(testFriend.scores[j]));
+                // If the sum of differences is less then the differences of the current "best match"
+                if (totalDifference <= bestMatch.friendDifference) {
+                    // Reset the bestMatch to be the new friend. 
+                    bestMatch.name = testFriend.name;
+                    bestMatch.photo = testFriend.photo;
+                    bestMatch.friendDifference = totalDifference;
+                    logStr += " ** current best match **"
+                }
+            }
+            console.log(logStr);
+        }
+
+        // The push method use to save user's data to the database
+        friends.push(userData);
+
+        //The res.json method will return a JSON data with the user's match which was looped through frieds data array. 
+        res.json(bestMatch);
+    });
 
 }
